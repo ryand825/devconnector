758bf832f48afc5a76ea5c21a926e0218dfd85fc
ryand88 <ryand8@hotmail.com>
2018-04-22 22:50:57 -0600

    Setting up passport

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb://ryan:ryan@ds247449.mlab.com:47449/devconnector1"
+  mongoURI: "mongodb://ryan:ryan@ds247449.mlab.com:47449/devconnector1",
+  secretOrKey: "secret"
 };
