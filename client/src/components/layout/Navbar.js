50b9fba1108ab2feae8b000dff075cf03d3e18fa
ryand88 <ryand8@hotmail.com>
2018-04-27 18:21:43 -0600

    Added dashboard link

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -16,6 +16,9 @@ export class Navbar extends Component {
 
     const authLinks = (
       <ul className="navbar-nav ml-auto">
+        <Link to="/Dashboard" className="nav-link">
+          Dashboard
+        </Link>
         <li className="nav-item">
           <a onClick={this.onLogoutClick} className="nav-link">
             <img
