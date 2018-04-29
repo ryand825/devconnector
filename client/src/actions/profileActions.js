ae41adbf6e409a3c4a7749127953c983f9f6664a
ryand88 <ryand8@hotmail.com>
2018-04-28 21:28:52 -0600

    added a profile action

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -17,6 +17,15 @@ export const getCurrentProfile = () => dispatch => {
     .catch(res => dispatch({ type: GET_PROFILE, payload: {} }));
 };
 
+// Get profile by handle
+export const getProfileByHandle = handle => dispatch => {
+  dispatch(setProfileLoading());
+  axios
+    .get(`/api/profile/handle/${handle}`)
+    .then(res => dispatch({ type: GET_PROFILE, payload: res.data }))
+    .catch(res => dispatch({ type: GET_PROFILE, payload: null }));
+};
+
 // Create Profile
 export const createProfile = (profileData, history) => dispatch => {
   axios
