27e9906bfd57c01e3dc73b8e38a9c5302ccc89b4
ryand88 <ryand8@hotmail.com>
2018-04-29 11:53:10 -0600

    keroku testing

--- config/keys.js
+++ config/keys.js
@@ -1,10 +1,5 @@
-module.exports = {
-  mongoURI: "mongodb://ryan:ryan@ds247449.mlab.com:47449/devconnector1",
-  secretOrKey: "secret"
-};
-
-// if (process.env.NODE_ENV === "production") {
-//   module.exports = require("./keys_prod");
-// } else {
-//   module.exports = require("./keys_dev");
-// }
+if (process.env.NODE_ENV === "production") {
+  module.exports = require("./keys_prod");
+} else {
+  module.exports = require("./keys_dev");
+}
