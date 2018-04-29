ad5aed5ceab977840061b143918b4b9835206a45
ryand88 <ryand8@hotmail.com>
2018-04-28 23:44:18 -0600

    Added "Like" and remove "Like" functionality

--- client/src/components/posts/PostItem.js
+++ client/src/components/posts/PostItem.js
@@ -3,40 +3,70 @@ import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import classnames from "classnames";
 import { Link } from "react-router-dom";
-import { deletePost } from "../../actions/postActions";
+import { deletePost, addLike, removeLike } from "../../actions/postActions";
 
 export class PostItem extends Component {
   onDeleteClick = id => {
     this.props.deletePost(id);
   };
 
+  onLikeClick = id => {
+    this.props.addLike(id);
+  };
+
+  onUnlikeClick = id => {
+    this.props.removeLike(id);
+  };
+
+  findUserLike = likes => {
+    const { auth } = this.props;
+
+    if (likes.filter(like => like.user === auth.user.id).length > 0) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+
   render() {
     const { post, auth } = this.props;
 
     return (
-      <div class="card card-body mb-3">
-        <div class="row">
-          <div class="col-md-2">
+      <div className="card card-body mb-3">
+        <div className="row">
+          <div className="col-md-2">
             <a href="profile.html">
               <img
-                class="rounded-circle d-none d-md-block"
+                className="rounded-circle d-none d-md-block"
                 src={post.avatar}
                 alt=""
               />
             </a>
             <br />
-            <p class="text-center">{post.name}</p>
+            <p className="text-center">{post.name}</p>
           </div>
-          <div class="col-md-10">
-            <p class="lead">{post.text}</p>
-            <button type="button" class="btn btn-light mr-1">
-              <i class="text-info fas fa-thumbs-up" />
-              <span class="badge badge-light">{post.likes.length}</span>
+          <div className="col-md-10">
+            <p className="lead">{post.text}</p>
+            <button
+              onClick={() => this.onLikeClick(post._id)}
+              type="button"
+              className="btn btn-light mr-1"
+            >
+              <i
+                className={classnames("fas fa-thumbs-up", {
+                  "text-info": this.findUserLike(post.likes)
+                })}
+              />
+              <span className="badge badge-light">{post.likes.length}</span>
             </button>
-            <button type="button" class="btn btn-light mr-1">
-              <i class="text-secondary fas fa-thumbs-down" />
+            <button
+              onClick={() => this.onUnlikeClick(post._id)}
+              type="button"
+              className="btn btn-light mr-1"
+            >
+              <i className="text-secondary fas fa-thumbs-down" />
             </button>
-            <Link to={`/post/${post._id}`} class="btn btn-info mr-1">
+            <Link to={`/post/${post._id}`} className="btn btn-info mr-1">
               Comments
             </Link>
             {post.user === auth.user.id ? (
@@ -58,11 +88,15 @@ export class PostItem extends Component {
 PostItem.propTypes = {
   auth: PropTypes.object.isRequired,
   post: PropTypes.object.isRequired,
-  deletePost: PropTypes.func.isRequired
+  deletePost: PropTypes.func.isRequired,
+  addLike: PropTypes.func.isRequired,
+  removeLike: PropTypes.func.isRequired
 };
 
 const mapStateToProps = state => ({
   auth: state.auth
 });
 
-export default connect(mapStateToProps, { deletePost })(PostItem);
+export default connect(mapStateToProps, { deletePost, addLike, removeLike })(
+  PostItem
+);
