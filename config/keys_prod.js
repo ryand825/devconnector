e8c330a06730a8c6c5599bc48c3362557984e312
ryand88 <ryand8@hotmail.com>
2018-04-29 12:02:40 -0600

    fixed config keys

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -1,4 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb://ryan:ryan@ds247449.mlab.com:47449/devconnector1",
-  secretOrKey: "secret"
+  mongoURI: process.env.MONGO_URI,
+  secretOrKey: process.env.SECRET_OR_KEY
 };
