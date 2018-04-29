4bddc8a3a571a6e2cd610964048641fe02ced185
ryand88 <ryand8@hotmail.com>
2018-04-29 11:40:32 -0600

    heroku testing

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,10 @@
-if (process.env.NODE_ENV === "production") {
-  module.exports = require("./keys_prod");
-} else {
-  module.exports = require("./keys_dev");
-}
+module.exports = {
+  mongoURI: "mongodb://ryan:ryan@ds247449.mlab.com:47449/devconnector1",
+  secretOrKey: "secret"
+};
+
+// if (process.env.NODE_ENV === "production") {
+//   module.exports = require("./keys_prod");
+// } else {
+//   module.exports = require("./keys_dev");
+// }
